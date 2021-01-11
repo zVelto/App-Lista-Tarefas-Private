75021e15708a21ea75eeb30efd602bc6721f30c8
Welton Carvalho <weltoncarvalhoo@gmail.com>
2021-01-11 18:09:36 -0300

    Listando todos os registros parte 2

--- tarefa.service.php
+++ tarefa.service.php
@@ -20,7 +20,16 @@ class TarefaService {
 	}
 
 	public function recuperar() { //read
-
+		$query = '
+			select 
+				t.id, s.status, t.tarefa 
+			from 
+				tb_tarefas as t
+				left join tb_status as s on (t.id_status = s.id)
+		';
+		$stmt = $this->conexao->prepare($query);
+		$stmt->execute();
+		return $stmt->fetchAll(PDO::FETCH_OBJ);
 	}
 
 	public function atualizar() { //update
--- tarefa_controller.php
+++ tarefa_controller.php
@@ -19,7 +19,12 @@
 		header('Location: nova_tarefa.php?inclusao=1');
 	
 	} else if($acao == 'recuperar') {
-		echo 'Chegamos até aqui!';
+		
+		$tarefa = new Tarefa();
+		$conexao = new Conexao();
+
+		$tarefaService = new TarefaService($conexao, $tarefa);
+		$tarefas = $tarefaService->recuperar();
 	}
 
 ?>
