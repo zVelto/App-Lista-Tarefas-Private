486b1826aa8c48b6d4230c4e52172dd7e14a3177
Welton Carvalho <weltoncarvalhoo@gmail.com>
2021-01-11 17:29:12 -0300

    Listando todos os registros parte 1

--- tarefa_controller.php
+++ tarefa_controller.php
@@ -4,14 +4,22 @@
 	require "../../app_lista_tarefas/tarefa.service.php";
 	require "../../app_lista_tarefas/conexao.php";
 
-	$tarefa = new Tarefa();
-	$tarefa->__set('tarefa', $_POST['tarefa']);
 
-	$conexao = new Conexao();
+	$acao = isset($_GET['acao']) ? $_GET['acao'] : $acao;
 
-	$tarefaService = new TarefaService($conexao, $tarefa);
-	$tarefaService->inserir();
+	if($acao == 'inserir' ) {
+		$tarefa = new Tarefa();
+		$tarefa->__set('tarefa', $_POST['tarefa']);
 
-	header('Location: nova_tarefa.php?inclusao=1');
+		$conexao = new Conexao();
+
+		$tarefaService = new TarefaService($conexao, $tarefa);
+		$tarefaService->inserir();
+
+		header('Location: nova_tarefa.php?inclusao=1');
+	
+	} else if($acao == 'recuperar') {
+		echo 'Chegamos até aqui!';
+	}
 
 ?>
