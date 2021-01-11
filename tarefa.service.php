11dd894c70f967169dc9b9fbfda66fab003111b1
Welton Carvalho <weltoncarvalhoo@gmail.com>
2021-01-11 16:37:35 -0300

    Inserindo registros

--- tarefa.service.php
+++ tarefa.service.php
@@ -3,8 +3,20 @@
 
 //CRUD
 class TarefaService {
-	public function inserir() { //create
 
+	private $conexao;
+	private $tarefa;
+
+	public function __construct(Conexao $conexao, Tarefa $tarefa) {
+		$this->conexao = $conexao->conectar();
+		$this->tarefa = $tarefa;
+	}
+
+	public function inserir() { //create
+		$query = 'insert into tb_tarefas(tarefa)values(:tarefa)';
+		$stmt = $this->conexao->prepare($query);
+		$stmt->bindValue(':tarefa', $this->tarefa->__get('tarefa'));
+		$stmt->execute();
 	}
 
 	public function recuperar() { //read
