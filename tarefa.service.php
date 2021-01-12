1913690238c0c4b063b0a6a751c6c803c810456e
Welton Carvalho <weltoncarvalhoo@gmail.com>
2021-01-12 18:06:32 -0300

    listando tarefas pendentes

--- tarefa.service.php
+++ tarefa.service.php
@@ -57,6 +57,22 @@ class TarefaService {
 		$stmt->bindValue(2, $this->tarefa->__get('id'));
 		return $stmt->execute(); 
 	}
+
+	public function recuperarTarefasPendentes() {
+		$query = '
+			select 
+				t.id, s.status, t.tarefa 
+			from 
+				tb_tarefas as t
+				left join tb_status as s on (t.id_status = s.id)
+			where
+				t.id_status = :id_status
+		';
+		$stmt = $this->conexao->prepare($query);
+		$stmt->bindValue(':id_status', $this->tarefa->__get('id_status'));
+		$stmt->execute();
+		return $stmt->fetchAll(PDO::FETCH_OBJ);
+	}
 }
 
 ?>
--- tarefa_controller.php
+++ tarefa_controller.php
@@ -36,7 +36,12 @@
 
 		$tarefaService = new TarefaService($conexao, $tarefa);
 		if($tarefaService->atualizar()) {
-			header('location: todas_tarefas.php');
+			
+			if( isset($_GET['pag']) && $_GET['pag'] == 'index') {
+				header('location: index.php');	
+			} else {
+				header('location: todas_tarefas.php');
+			}
 		}
 
 
@@ -50,7 +55,11 @@
 		$tarefaService = new TarefaService($conexao, $tarefa);
 		$tarefaService->remover();
 
-		header('location: todas_tarefas.php');
+		if( isset($_GET['pag']) && $_GET['pag'] == 'index') {
+			header('location: index.php');	
+		} else {
+			header('location: todas_tarefas.php');
+		}
 	
 	} else if($acao == 'marcarRealizada') {
 
@@ -62,7 +71,21 @@
 		$tarefaService = new TarefaService($conexao, $tarefa);
 		$tarefaService->marcarRealizada();
 
-		header('location: todas_tarefas.php');
+		if( isset($_GET['pag']) && $_GET['pag'] == 'index') {
+			header('location: index.php');	
+		} else {
+			header('location: todas_tarefas.php');
+		}
+	
+	} else if($acao == 'recuperarTarefasPendentes') {
+		$tarefa = new Tarefa();
+		$tarefa->__set('id_status', 1);
+		
+		$conexao = new Conexao();
+
+		$tarefaService = new TarefaService($conexao, $tarefa);
+		$tarefas = $tarefaService->recuperarTarefasPendentes();
 	}
+
 
 ?>
